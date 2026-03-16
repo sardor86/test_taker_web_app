8bf805d364b83c4385d6e7ffb20f5be640d8fbf2
systemhub144 <systemhub144@gmail.com>
2026-03-16 06:22:03 +0500

    added certificate pdf prepare class and results pdf prepare class

--- app/tg_bot/handlers/admin.py
+++ app/tg_bot/handlers/admin.py
@@ -26,6 +26,8 @@ admin_router = Router()
 
 class Certificate:
     def __init__(self):
+        font_path = PATH / 'app/tg_bot/certificates/DejaVuSans-Bold.ttf'
+        pdfmetrics.registerFont(TTFont('DejaVuSans-Bold', font_path))
 
         self.CERTIFICATE_LIST = [
             PdfReader(open(PATH / 'app/tg_bot/certificates/0.pdf', 'rb')).pages[0],
@@ -79,42 +81,11 @@ class Certificate:
         self.writer.write(self.certificate)
         self.result_file = BufferedInputFile(self.certificate.getvalue(), filename='certificate.pdf')
 
-    async def prepare_certificate(self):
+    def prepare_certificate(self):
         self.prepare_text_of_full_name()
         self.canvas.save()
         self.prepare_overlay_for_pdf_file()
         self.prepare_certificate_file()
-
-
-
-async def prepare_certificate(certificate, full_name: str) -> BufferedInputFile:
-    overlay = BytesIO()
-    c = canvas.Canvas(overlay, pagesize=(595, 842))
-
-    font_name = "DejaVuSans-Bold"
-    font_size = 28
-    y = 410
-
-    text_width = pdfmetrics.stringWidth(full_name, font_name, font_size)
-    x = (549 - text_width) / 2
-
-    c.setFont(font_name, font_size)
-    c.drawString(x, y, full_name)
-    c.save()
-
-    overlay.seek(0)
-
-    overlay_pdf = PdfReader(overlay)
-    writer = PdfWriter()
-
-    page = copy.copy(certificate)
-    page.merge_page(overlay_pdf.pages[0])
-    writer.add_page(page)
-
-    result = BytesIO()
-    writer.write(result)
-
-    return BufferedInputFile(result.getvalue(), filename='certificate.pdf')
 
 
 class PDFResultsTable:
@@ -124,8 +95,11 @@ class PDFResultsTable:
         self.BORDER = 1
 
         self.pdf = FPDF()
+
+        self.session = None
         self.data = None
         self.file_name = None
+        self.result_file = None
 
     def set_font_for_column(self):
         self.pdf.set_font('Arial', 'B', 16)
@@ -139,18 +113,19 @@ class PDFResultsTable:
         self.pdf.cell(self.COLUMN_WIDTH, self.ROW_HEIGHT, 'Score', self.BORDER)
         self.pdf.ln(10)
 
-    async def draw_one_line_of_data(self, user_data):
+    async def draw_one_line_of_data(self, attempt):
+        user_data = await get_user_data(user_id=attempt.user_id, async_session_maker=self.session)
         self.pdf.cell(self.COLUMN_WIDTH, self.ROW_HEIGHT, user_data.username, self.BORDER)
         self.pdf.cell(self.COLUMN_WIDTH, self.ROW_HEIGHT, user_data.lastname, self.BORDER)
-        self.pdf.cell(self.COLUMN_WIDTH, self.ROW_HEIGHT, str(user_data.score), self.BORDER)
+        self.pdf.cell(self.COLUMN_WIDTH, self.ROW_HEIGHT, str(attempt.score), self.BORDER)
         self.pdf.ln(10)
 
     async def draw_all_user_data(self):
         for user in self.data:
             await self.draw_one_line_of_data(user)
 
-    def prepare_pdf_output(self) -> BufferedInputFile:
-        return BufferedInputFile(self.pdf.output('S').encode('latin-1'), filename='results.pdf')
+    def prepare_pdf_output(self):
+        self.result_file = BufferedInputFile(self.pdf.output('S').encode('latin-1'), filename='results.pdf')
 
     async def create_pdf_table(self):
         self.pdf.add_page()
@@ -158,60 +133,30 @@ class PDFResultsTable:
         self.create_columns_for_table()
         self.set_font_for_data()
         await self.draw_all_user_data()
-
-        return self.prepare_pdf_output()
-
-
-async def create_pdf_table(data, file_name, session):
-    # Создание PDF-документа
-    pdf = FPDF()
-    pdf.add_page()
-
-    # Создание таблицы
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(40, 10, 'First Name', 1)
-    pdf.cell(40, 10, 'Last Name', 1)
-    pdf.cell(40, 10, 'Age', 1)
-    pdf.ln(10)
-
-    pdf.set_font('Arial', '', 12)
-    for i, attempt in enumerate(data):
-        user_data = await get_user_data(user_id=attempt.user_id, async_session_maker=session)
-        pdf.cell(40, 10, user_data.username, 1)
-        pdf.cell(40, 10, user_data.lastname, 1)
-        pdf.cell(40, 10, str(attempt.score), 1)
-        pdf.ln(10)
-
-    # Сохранение PDF-файла
-    pdf.output(file_name, 'F')
+        self.prepare_pdf_output()
 
 
 async def test_results_message_parts(test_id: int, session: AsyncSession, redis: Redis) -> list:
-    results = (await get_all_users_results(20,
-                                           async_session_maker=session))
-
-    await create_pdf_table(results, 'something.pdf', session)
     test_info = await get_test_info(test_id, async_session_maker=session, redis=redis)
 
     message_parts = ['Test natijalari:\n\n'
                      f'Test nomi: {test_info["test_name"]}\n'
-                     f'Test kodi: {test_id}\n\n']
-
-    font_path = PATH / 'app/tg_bot/certificates/DejaVuSans-Bold.ttf'
-    pdfmetrics.registerFont(TTFont('DejaVuSans-Bold', font_path))
-
-    medals = ['🥇', '🥈', '🥉']
-    for i, attempt in enumerate(results):
-        user_data = await get_user_data(user_id=attempt.user_id, async_session_maker=session)
-        full_name = f'{user_data.username} {user_data.username}'
-        medal = medals[i] if i < len(medals) else ''
-        message_parts.append(f'{i + 1}: {full_name} - {attempt.score} ball {medal}')
-
-    message_parts.append('\n\nToʻgʻri javoblar: ')
+                     f'Test kodi: {test_id}\n\n'
+                     '\n\nToʻgʻri javoblar: ']
 
     answers = await get_test_answers(test_id, async_session_maker=session)
-    for answer in answers:
-        message_parts.append(f'{answer.question_number} - {answer.correct_answer}')
+    per_row = 6
+    rows = []
+    for i in range(0, len(answers), per_row):
+        part = answers[i:i + per_row]
+        row = []
+        for j, ans in enumerate(part, start=i + 1):
+            row.append(f"{j}.{ans.correct_answer}".ljust(10))
+        rows.append("".join(row))
+
+    table = "\n".join(rows)
+    message_answer_table = f"```\n{table}\n```"
+    message_parts.append(message_answer_table)
 
     message_parts.append('\nTestda ishtirok etgan barchaga rahmat😊')
     return message_parts
@@ -221,17 +166,17 @@ async def send_certificates(test_id, bot: Bot, session: AsyncSession, redis: Red
     results = (await get_all_users_results(test_id,
                                            async_session_maker=session))
     test_info = await get_test_info(test_id, async_session_maker=session, redis=redis)
-
-    certificate_list = [
-        PdfReader(open(PATH / 'app/tg_bot/certificates/0.pdf', 'rb')).pages[0],
-        PdfReader(open(PATH / 'app/tg_bot/certificates/1.pdf', 'rb')).pages[0],
-        PdfReader(open(PATH / 'app/tg_bot/certificates/2.pdf', 'rb')).pages[0],
-        PdfReader(open(PATH / 'app/tg_bot/certificates/3.pdf', 'rb')).pages[0],
-    ]
+    certificate = Certificate()
 
     for i, attempt in enumerate(results):
         user_data = await get_user_data(user_id=attempt.user_id, async_session_maker=session)
         full_name = f'{user_data.lastname} {user_data.username}'
+
+        certificate.full_name = full_name
+        certificate.set_certificate_type_by_place(i)
+        certificate.prepare_certificate()
+        certificate_file = certificate.result_file
+
         try:
             await bot.send_message(chat_id=attempt.tg_user_id,
                                    text=f'Testda qatnashganingiz uchun rahmat,\n'
@@ -244,18 +189,30 @@ async def send_certificates(test_id, bot: Bot, session: AsyncSession, redis: Red
         except TelegramBadRequest:
             pass
 
-        if i <= 2:
-            file = await prepare_certificate(certificate_list[i], full_name)
-        else:
-            file = await prepare_certificate(certificate_list[3], full_name)
-
         try:
-            await bot.send_document(chat_id=attempt.tg_user_id, document=file)
+            await bot.send_document(chat_id=attempt.tg_user_id, document=certificate_file)
         except TelegramForbiddenError:
             pass
         except TelegramBadRequest:
             pass
 
+
+async def send_results(test_id, callback: CallbackQuery, session: AsyncSession):
+    results = await get_all_users_results(test_id, async_session_maker=session)
+    results_table = PDFResultsTable()
+    results_table.session = session
+    results_table.file_name = 'results.pdf'
+    results_table.data = results
+    results_table.prepare_pdf_output()
+
+    results_file = results_table.result_file
+
+    try:
+        await callback.bot.send_document(chat_id=callback.message.chat.id, document=results_file)
+    except TelegramForbiddenError:
+        pass
+    except TelegramBadRequest:
+        pass
 
 @admin_router.callback_query(F.data.split('::')[0] == 'stop_test')
 async def stop_test(callback: CallbackQuery) -> None:
@@ -284,6 +241,7 @@ async def stop_test(callback: CallbackQuery) -> None:
                             session=callback.bot.async_session_maker,
                             redis=callback.bot.redis,
                             bot=callback.bot)
+    await send_results(test_id=test_id, callback=callback, session=callback.bot.async_session_maker)
 
 
 @admin_router.callback_query(F.data.split('::')[0] == 'get_results_test')
@@ -292,13 +250,13 @@ async def get_results_test(callback: CallbackQuery) -> None:
     message_parts = await test_results_message_parts(test_id=test_id,
                                                      session=callback.bot.async_session_maker,
                                                      redis=callback.bot.redis)
-
     try:
         await callback.message.reply(text='\n'.join(message_parts))
     except TelegramForbiddenError:
         pass
     except TelegramBadRequest:
         pass
+    await send_results(test_id=test_id, callback=callback, session=callback.bot.async_session_maker)
 
 
 @admin_router.callback_query(F.data.split('::')[0] == 'allow_admin')
